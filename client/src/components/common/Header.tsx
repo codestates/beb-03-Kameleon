5dc0c056c62092c833681a159e86f05f99b2390d
EllaDev <ellawork23@gmail.com>
2022-05-10 12:30:12 +0900

    style(윤은주): apply header style

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,7 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import logoImage from '../../assets/images/logo.png';
+
+const HeaderStyle = styled.header`
+  width: 100%;
+  background-color: #276955;
+
+  .layout {
+    display: flex;
+    align-items: center;
+    justify-content: left;
+    width: 100%;
+    height: 80px;
+  }
+
+  .utils {
+    display: block;
+    margin-right: auto;
+  }
+`;
+
+const LogoStyle = styled.h1`
+  width: 140px;
+  a {
+    display: block;
+    padding: 9px 0;
+  }
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const NavStyle = styled.nav`
+  width: 80%;
+  height: 100%;
+  ul {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+  li {
+    padding: 0 15px;
+    a {
+      display: block;
+      color: #fff;
+      font-size: 1.3rem;
+    }
+  }
+`;
 
 const Header = () => {
-  return <div></div>;
+  const [isLogin, setIsLogin] = useState(true);
+
+  return (
+    <HeaderStyle>
+      <div className="layout">
+        <LogoStyle>
+          <Link to="/">
+            <img src={logoImage} alt="logo" />
+          </Link>
+        </LogoStyle>
+        <NavStyle>
+          <ul>
+            <li>
+              <Link to="/swap">Swap</Link>
+            </li>
+            <li>
+              <Link to="/pool">Pool</Link>
+            </li>
+            <li>
+              <Link to="/mint">Mint&Burn</Link>
+            </li>
+            <li>
+              <Link to="/govern">Govern</Link>
+            </li>
+          </ul>
+        </NavStyle>
+        <span className="utils">
+          {isLogin ? (
+            <button type="button">Connect</button>
+          ) : (
+            <button type="button">MyPage</button>
+          )}
+        </span>
+      </div>
+    </HeaderStyle>
+  );
 };
 
 export default Header;
