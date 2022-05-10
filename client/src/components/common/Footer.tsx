3864af537d55b8bb6256729fc93f15caf4ba8b25
EllaDev <ellawork23@gmail.com>
2022-05-11 00:22:23 +0900

    style(윤은주) : apply footer style

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const FooterStyle = styled.footer`
+  padding: 12px 0;
+  width: 100%;
+  text-align: center;
+  color: #fff;
+
+  a {
+    color: #fff;
+    margin-left: 5px;
+  }
+`;
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterStyle>
+      copyright(c) 2022 All rights reserved by Kameleon |
+      <a href="https://github.com/codestates/beb-03-Kameleon">
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+    </FooterStyle>
+  );
 };
 
 export default Footer;
