e99eafe5159af1cfc5666692e980602d71fbde24
EllaDev <ellawork23@gmail.com>
2022-05-11 15:37:25 +0900

    feat(윤은주): markup mint page

---
+++ client/src/pages/styles/MintPage.styles.tsx
@@ -0,0 +1,133 @@
+import styled from 'styled-components';
+
+const MintPageWrapper = styled.div`
+  max-width: var(--s-width);
+  margin: auto;
+  position: relative;
+
+  & > h2 {
+    margin: 1rem;
+    color: var(--white);
+  }
+
+  & > form {
+    padding: 1rem 2rem;
+    background-color: var(--white);
+    border-radius: 1rem;
+
+    & > button {
+      display: block;
+      padding: 1rem 2rem;
+      margin: auto;
+      margin-top: 1.5rem;
+      width: 100%;
+      color: var(--white);
+      font-weight: 700;
+      font-size: 1rem;
+      background-color: var(--dark-green);
+      border-radius: 1rem;
+      border: 0;
+    }
+  }
+`;
+
+const TabStyle = styled.span`
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: var(--gray);
+  border-radius: 4rem;
+  overflow: hidden;
+
+  & > button {
+    padding: 0.3rem 0.6rem;
+    color: var(--white);
+    font-size: 0.7rem;
+    border: 0;
+    border-radius: 4rem;
+    background-color: transparent;
+
+    &.on {
+      background-color: var(--dark-green);
+    }
+  }
+`;
+
+const InputStyle = styled.div`
+  padding: 0.8rem 1.6rem;
+  background-color: var(--green);
+  border-radius: 0.5rem;
+
+  & > div {
+    position: relative;
+    width: 100%;
+    padding: 0.5rem;
+    margin-top: 0.5rem;
+    background-color: var(--white);
+
+    & > label {
+      display: block;
+      width: 100%;
+      color: var(--gray);
+      font-size: 0.8rem;
+      font-weight: 700;
+    }
+
+    & > span {
+      display: flex;
+
+      & em {
+        font-style: normal;
+        font-size: 0.8rem;
+        font-weight: 700;
+        color: var(--green);
+      }
+
+      & input {
+        width: 100%;
+        border: 0;
+        margin-top: 0.3rem;
+      }
+    }
+  }
+  & > dl {
+    padding: 0.5rem;
+    margin-top: 2rem;
+    background-color: var(--white);
+    font-size: 0.8rem;
+    font-weight: 700;
+
+    & > dt {
+      color: var(--gray);
+    }
+    & > dd {
+      margin-top: 0.3rem;
+      color: var(--green);
+    }
+  }
+`;
+
+const DetailInfoStyle = styled.dl`
+  margin-top: 1.5rem;
+  padding: 1rem;
+  border-top: 1px solid var(--green);
+
+  & > div {
+    display: flex;
+    font-size: 0.8rem;
+  }
+
+  & dt {
+    width: 35%;
+    color: var(--gray);
+  }
+
+  & dd {
+    width: 65%;
+    color: var(--green);
+    font-weight: 700;
+    text-align: right;
+  }
+`;
+
+export { MintPageWrapper, InputStyle, DetailInfoStyle, TabStyle };
