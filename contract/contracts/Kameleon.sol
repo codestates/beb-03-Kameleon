16f2d3d21b50996d063658fba14906a82bfec7c7
lhj6102 <lhj6102@gmail.com>
2022-05-11 18:08:38 +0900

    feat: Kameleon v2.1

--- contract/contracts/Kameleon.sol
+++ contract/contracts/Kameleon.sol
@@ -40,29 +40,6 @@ contract Kameleon is KIP7, Factory, Ownable {
     // last대비 상승한 current LPT
     mapping(address => uint256) private _increasedLPT;
 
-
-    // testMintToLPs 용 test amount
-    // uint256 private _testAmount = 1000000000000000000;
-    
-
-    /**
-     * test: testMintToLPs(): 컨트랙트 내에 등록된 address에 민트
-     */
-     /** 
-    function testMintToLPs() public onlyOwner {
-        require(exchangeCount > 0, "testMintToLPs: no Exchange exists");
-        // 전체 Exchange 탐색
-        for (uint256 i = 0; i < exchangeCount; i++) {
-            // Exchange 하나 선택
-            Exchange exchange = Exchange(exchangeList[i]);
-            for (uint256 j = 0; j < exchange.getAddrCount(); j++) {
-                // Exchange 내의 address 하나 선택, 민팅
-                _mint(exchange.getAddr(j), _testAmount);
-            }
-        }
-    }
-     */
-
     // override Factory:createExchange
     function createExchange(address _tokenAddress) public onlyOwner returns (address) {
         address temp = super.createExchange(_tokenAddress);
