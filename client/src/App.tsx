6dadda94385f8e90324973a8c44ef04fc80843c7
JSC <gbs04087@gmail.com>
2022-05-26 15:06:56 +0900

    feat(client): add route animation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  Location,
+} from 'react-router-dom';
 import { GlobalStyle, AppStyle, MainStyle } from './GlobalStyle';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
@@ -10,37 +16,56 @@ import LiquidityPage from './pages/LiquidityPage';
 import MintPage from './pages/MintPage';
 import MyPage from './pages/MyPage';
 import GovernPage from './pages/GovernPage';
-// import { TransitionGroup, CSSTransition } from 'react-transition-group';
-
 import ParallaxContainer from './components/Parallax/ParallaxContainer';
+import React from 'react';
+
+const Animation = () => {
+  const location: Location = useLocation();
+  const [displayLocation, setDisplayLocation] =
+    React.useState<Location>(location);
+  const [transitionStage, setTransistionStage] =
+    React.useState<string>('fadeIn');
+  React.useEffect(() => {
+    if (location !== displayLocation) setTransistionStage('fadeOut');
+  }, [location, displayLocation]);
+  return (
+    <>
+      <div
+        className={`${transitionStage}`}
+        onAnimationEnd={() => {
+          if (transitionStage === 'fadeOut') {
+            setTransistionStage('fadeIn');
+            setDisplayLocation(location);
+          }
+        }}
+      >
+        <Routes location={displayLocation}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/swap" element={<SwapPage />} />
+          <Route path="/swap/:token" element={<SwapPage />} />
+          <Route path="/pool" element={<PoolPage />} />
+          <Route path="/liquidity/:id" element={<LiquidityPage />} />
+          <Route path="/mint" element={<MintPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/govern" element={<GovernPage />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
 
 function App() {
-  // const location = useLocation();
-
   return (
     <>
       <GlobalStyle />
       <AppStyle>
         <div>
           <BrowserRouter>
-            {/* <TransitionGroup component={null}>
-              <CSSTransition key={location.key} classNames="fade" timeout={300}> */}
             <Header />
             <MainStyle className="layout">
-              <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/swap" element={<SwapPage />} />
-                <Route path="/swap/:token" element={<SwapPage />} />
-                <Route path="/pool" element={<PoolPage />} />
-                <Route path="/liquidity/:id" element={<LiquidityPage />} />
-                <Route path="/mint" element={<MintPage />} />
-                <Route path="/mypage" element={<MyPage />} />
-                <Route path="/govern" element={<GovernPage />} />
-              </Routes>
+              <Animation />
             </MainStyle>
             <Footer />
-            {/* </CSSTransition>
-            </TransitionGroup> */}
           </BrowserRouter>
         </div>
         <ParallaxContainer />
